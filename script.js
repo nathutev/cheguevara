e10d17d2a99f339e387f558415f725f16fd0ab3d
Natan Utevsky <99504834+nathutev@users.noreply.github.com>
2022-12-30 18:35:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 } 
